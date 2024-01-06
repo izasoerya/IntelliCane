1c1705a21f0eaadd722ae3e8ba4a8b2a6b7ac4c1
izasoerya <ihza.surya@gmail.com>
2024-01-07 01:22:55 +0700

    in the meantime so home screen looks nice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,87 @@
 import 'package:flutter/material.dart';
-import 'maps_page/page.dart';
 
-void main() => runApp(const MainPageMaps());
+void main() => runApp(const MainPageApplication());
+
+class MainPageApplication extends StatelessWidget {
+  const MainPageApplication({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maps Sample App',
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        elevation: 4.0,
+        child: Column(
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text('Achmad Taufiq'),
+              accountEmail: Text('Guardian'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.person),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.map),
+              title: const Text('Maps'),
+              onTap: () {
+                // Handle drawer item tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.message),
+              title: const Text('Message'),
+              onTap: () {
+                // Handle drawer item tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'IntelliCane',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 0, 75, 160),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 255, 255, 255),
+                  Color.fromARGB(255, 255, 255, 255)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          const Text(
+            "Welcome to IntelliCane",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
