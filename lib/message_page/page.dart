96eeedb07dbd01fb980a802e5be7db41c0d873d9
izasoerya <ihza.surya@gmail.com>
2024-01-10 15:47:59 +0700

    feat: comment out message

--- lib/message_page/page.dart
+++ lib/message_page/page.dart
@@ -1,54 +1,54 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'message_format.dart';
-import 'fcm.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'message_format.dart';
+// import '../config/fcm.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  State<StatefulWidget> createState() => _MyHomePageState();
-}
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+//   @override
+//   State<StatefulWidget> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final FCMHandler _fcmHandler = FCMHandler();
-  RemoteMessage temp = const RemoteMessage();
-  @override
-  void initState() {
-    super.initState();
-    _initFCM();
-  }
+// class _MyHomePageState extends State<MyHomePage> {
+//   final FCMHandler _fcmHandler = FCMHandler();
+//   RemoteMessage temp = const RemoteMessage();
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initFCM();
+//   }
 
-  void incomingMessage(RemoteMessage message) {
-    setState(() {
-      temp = message;
-    });
-  }
+//   void incomingMessage(RemoteMessage message) {
+//     setState(() {
+//       temp = message;
+//     });
+//   }
 
-  Future<void> _initFCM() async {
-    await _fcmHandler.initFCM(incomingMessage);
-  }
+//   Future<void> _initFCM() async {
+//     await _fcmHandler.initFCM();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    MessageStructure messageStructure = MessageStructure(message: temp);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FCM Example'),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Text(messageStructure.notificationTitle),
-            Text(messageStructure.notificationBody),
-            Text(messageStructure.dataTitle),
-            Text(messageStructure.dataLatitude),
-            Text(messageStructure.dataLongitude),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     MessageStructure messageStructure = MessageStructure(message: temp);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('FCM Example'),
+//       ),
+//       body: Container(
+//         alignment: Alignment.center,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           mainAxisSize: MainAxisSize.max,
+//           children: [
+//             Text(messageStructure.notificationTitle),
+//             Text(messageStructure.notificationBody),
+//             Text(messageStructure.dataTitle),
+//             Text(messageStructure.dataLatitude as String),
+//             Text(messageStructure.dataLongitude as String),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
