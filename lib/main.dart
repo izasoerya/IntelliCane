c5be1a503da088d0b2c394d97607073ea13b1336
izasoerya <ihza.surya@gmail.com>
2024-03-11 21:48:34 +0700

    riverpod init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'login_page/page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  runApp(const MainView());
+  runApp(
+    const ProviderScope(
+      child: MainView(),
+    ),
+  );
 }
