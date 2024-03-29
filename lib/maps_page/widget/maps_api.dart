1a80dfed346bdc41dd3d3547607ab074a4c8263b
izasoerya <ihza.surya@gmail.com>
2024-03-29 15:44:38 +0700

    polyline created

--- lib/maps_page/widget/maps_api.dart
+++ lib/maps_page/widget/maps_api.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_geojson/flutter_map_geojson.dart';
+// import 'package:flutter_map_tappable_polyline/flutter_map_tappable_polyline.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:intellicane/database/data_geojson_sleman.dart';
 
 class MapsAPI extends StatelessWidget {
   const MapsAPI({super.key});
 
   @override
   Widget build(BuildContext context) {
+    GeoJsonParser geoJsonParser = GeoJsonParser();
+    geoJsonParser.parseGeoJsonAsString(dataGeoJSON);
     return FlutterMap(
       options: const MapOptions(
-        initialCenter: LatLng(51.509364, -0.128928),
-        initialZoom: 9.2,
+        initialCenter: LatLng(-7.756165, 110.375403),
+        initialZoom: 13.1,
       ),
       children: [
         TileLayer(
           urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.example.app',
         ),
+        PolygonLayer(polygons: geoJsonParser.polygons),
+        PolylineLayer(polylines: geoJsonParser.polylines),
+        MarkerLayer(markers: geoJsonParser.markers),
         RichAttributionWidget(attributions: [
           TextSourceAttribution('© OpenStreetMap contributors',
               onTap: () => 'https://www.openstreetmap.org/copyright'),
